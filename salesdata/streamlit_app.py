0ab5e1be8f25b55ba8f7885469088228bb8f15f7
Siddhesh-Bankar <siddhesh.bankar@arieotech.com>
2025-02-06 12:50:14 +0530

    Added creds file

--- salesdata/streamlit_app.py
+++ salesdata/streamlit_app.py
@@ -17,6 +17,7 @@ from googleapiclient.errors import HttpError
 from tools.nl2sqltask import getnl2sqlQuery
 from tools.insighttask import getInsights
 from google.auth.transport.requests import Request
+from pathlib import Path
 
 
 st.set_page_config(page_title="Sales Data Insights", page_icon="📈", layout="centered")
@@ -52,7 +53,7 @@ def authenticate_gmail():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                r'C:\Users\siddhesh.bankar\Downloads\Desktop.json', SCOPES)
+                Path(__file__).parent / "Desktop.json", SCOPES)
             creds = flow.run_local_server(port=0)
         
         with open('token.json', 'w') as token:
